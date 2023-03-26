83f41ef88e517074904d7e3c25d919242833cb7b
JulianC <jacaminaur@gmail.com>
2023-03-26 13:49:08 +0200

    RouteServiceProvider::ERROR

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-    public const ERROR = 'error-page';
+    public const ERROR = '/error-page';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
